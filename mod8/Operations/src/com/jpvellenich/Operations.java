5a7c679df580c6b807b035d0906e612074d17e6a
JoaoVellenich <jpvellenich@gmail.com>
2024-01-15 14:23:56 -0300

    feat: mod8

---
+++ mod8/Operations/src/com/jpvellenich/Operations.java
@@ -0,0 +1,15 @@
+package com.jpvellenich;
+
+public class Operations {
+    public static void main(String[] args) {
+        double nota1 = 5.4;
+        double nota2 = 6.4;
+        double nota3 = 2.4;
+        double nota4 = 9.4;
+        double media = 0;
+
+        media = (nota3 + nota2 + nota1 + nota4) / 4;
+
+        System.out.println("A media das 4 notas é " + media);
+    }
+}
