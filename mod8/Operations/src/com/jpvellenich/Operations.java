0919a857ee620bb24c9cfbe05dd7be43a4a703df
JoaoVellenich <jpvellenich@gmail.com>
2024-01-24 18:19:28 -0300

    Feat: adding if

--- mod8/Operations/src/com/jpvellenich/Operations.java
+++ mod8/Operations/src/com/jpvellenich/Operations.java
@@ -2,13 +2,21 @@ package com.jpvellenich;
 
 public class Operations {
     public static void main(String[] args) {
-        double nota1 = 5.4;
+        double nota1 = 9.4;
         double nota2 = 6.4;
-        double nota3 = 2.4;
-        double nota4 = 9.4;
+        double nota3 = 0.4;
+        double nota4 = 0.4;
         double media = 0;
 
         media = (nota3 + nota2 + nota1 + nota4) / 4;
+
+        if(media >= 7){
+            System.out.println("Aprovado");
+        }else if(media >=5){
+            System.out.println("Recuperação");
+        }else {
+            System.out.println("Reprocado");
+        }
 
         System.out.println("A media das 4 notas é " + media);
     }
